e26dbd0049741ef780e7775e31f32c7c4ffcf99c
Carson Clark <carsonclark@mbp14.local>
2024-01-09 07:33:12 -0800

    add textfield and set delegate

--- Waterfall Finance/LoginView.swift
+++ Waterfall Finance/LoginView.swift
@@ -9,6 +9,9 @@ import Foundation
 import UIKit
 
 class LoginView: UIView {
+    
+    let userNameTextField = UITextField()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         style()
@@ -28,9 +31,34 @@ extension LoginView {
     func style() {
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .orange
+        userNameTextField.translatesAutoresizingMaskIntoConstraints = false
+        userNameTextField.placeholder = "Username"
+        // register this view for delegate of textField
+        // this means text field will send messages through its protocol
+        userNameTextField.delegate = self
     }
     
     func layout() {
         
+    }
+}
+
+//MARK: - UITextFieldDelegate
+extension LoginView: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        userNameTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        <#code#>
     }
 }
