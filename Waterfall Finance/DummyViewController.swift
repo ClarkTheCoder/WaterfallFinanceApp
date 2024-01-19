803949b91abfe6686936e8b1d8490da20626c9f4
Carson Clark <carsonclark@mbp14.local>
2024-01-19 05:19:17 -0800

    add dummy view controller to test logout & onboarding

---
+++ Waterfall Finance/DummyViewController.swift
@@ -0,0 +1,46 @@
+//
+//  DummyViewController.swift
+//  Waterfall Finance
+//
+//  Created by Carson Clark on 2024-01-19.
+//
+
+import UIKit
+
+class DummyViewController: UIViewController {
+    let stackView = UIStackView()
+    let label = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension DummyViewController {
+    func style() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Welcome"
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+        
+    }
+    
+    func layout() {
+        stackView.addArrangedSubview(label)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+                
+        ])
+        
+    }
+    
+}
