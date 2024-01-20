21c93af41e7852cc3c488b72c04ea12085d12166
Carson Clark <carsonclark@mbp14.local>
2024-01-20 02:19:48 -0800

    show onboarding only once

---
+++ Waterfall Finance/LocalState.swift
@@ -0,0 +1,24 @@
+//
+//  LocalState.swift
+//  Waterfall Finance
+//
+//  Created by Carson Clark on 2024-01-20.
+//
+
+import Foundation
+
+public class LocalState {
+    
+    private enum Keys: String {
+        case hasOnboarded
+    }
+    
+    public static var hasOnboarded: Bool {
+        get {
+            return UserDefaults.standard.bool(forKey: Keys.hasOnboarded.rawValue)
+        }
+        set(newValue) {
+            UserDefaults.standard.set(newValue, forKey: Keys.hasOnboarded.rawValue)
+        }
+    }
+}
