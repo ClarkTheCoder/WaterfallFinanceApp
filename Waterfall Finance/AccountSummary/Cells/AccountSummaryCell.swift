b4abd0e30d27f7011f2773aba0fde86d650639eb
Carson Clark <carsonclark@mbp14.local>
2024-01-25 21:25:05 -0800

    create and style AccountSummaryCell

---
+++ Waterfall Finance/AccountSummary/Cells/AccountSummaryCell.swift
@@ -0,0 +1,44 @@
+//
+//  AccountSummaryCell.swift
+//  Waterfall Finance
+//
+//  Created by Carson Clark on 2024-01-25.
+//
+
+import UIKit
+
+class AccountSummaryCell: UITableViewCell {
+    
+    let typeLabel = UILabel()
+    static let reuseID = "AccountSummaryCell"
+    static let rowHeight: CGFloat = 100
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension AccountSummaryCell {
+    func setup() {
+        typeLabel.translatesAutoresizingMaskIntoConstraints = false
+        typeLabel.font = UIFont.preferredFont(forTextStyle: .caption1)
+        typeLabel.adjustsFontForContentSizeCategory = true
+        typeLabel.text = "Account type"
+        
+        contentView.addSubview(typeLabel)
+        
+    }
+    
+    func layout() {
+        NSLayoutConstraint.activate([
+            typeLabel.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 2),
+            typeLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2)
+        ])
+    }
+}
